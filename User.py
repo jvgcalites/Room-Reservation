0f1f09945d26dd92ba7357dc9694e6b7f57273e0
jvgcalites <jvgcalites2018@gmail.com>
2018-10-01 04:19:47 +0800

    shows the organization automatically upon opening of userpage

--- User.py
+++ User.py
@@ -5,27 +5,34 @@
 # Created by: PyQt5 UI code generator 5.9.2
 #
 # WARNING! All changes made in this file will be lost! 
-
+from Reservation_Filehandling import Reservation_fileHandling
 from PyQt5.QtWidgets import QApplication, QMessageBox, QMainWindow
 from PyQt5.uic import loadUi
 
 class User(QMainWindow):
-    def __init__(self):
+    def __init__(self, email):
+        self.email = email
         super(User, self).__init__()
         loadUi('User.ui',self)
+        
+        self.rfh = Reservation_fileHandling() 
+        self.rfh.LoadDatabase()
+        self.lineEdit_Organization.setText(self.rfh.GetOrganization(self.email))
+        self.rfh.CloseDatabase()
+        
         self.loginMsg = QMessageBox()
         self.setWindowTitle('Room Reservation')
-     
-        #Button Events
-        self.pushButton_Reserve.clicked.connect(self.Reserve_Clicked)
 
+        #Button Events
+        self.pushButton_Reserve.clicked.connect(lambda: self.Reserve_Clicked())
+        
     def Reserve_Clicked(self):
         print("DAYNE")
-    
+        
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
-    widget = User()
+    widget = User("acsabesamis@mymail.mapua.edu.ph")
     widget.show()
     sys.exit(app.exec_())
 
