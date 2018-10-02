a898015993605aef35869ffc07759ba540099ed7
jvgcalites <jvgcalites2018@gmail.com>
2018-10-02 23:22:18 +0800

    Reserve button now working

--- User.py
+++ User.py
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
 # -*- coding: utf-8 -*-
 
 from Reservation_Filehandling import Reservation_fileHandling
 from PyQt5.QtWidgets import QApplication, QMessageBox, QMainWindow
 from PyQt5.uic import loadUi
+from PyQt5 import QtCore
 from Reservation import Reservation
 
 class User(QMainWindow):
@@ -21,25 +21,54 @@ class User(QMainWindow):
         self.lineEdit_Organization.setText(self.rfh.GetOrganization(self.email))
 
         #Button Events
-        self.pushButton_Reserve.clicked.connect(lambda: self.Reserve_Clicked())
-            
+        self.pushButton_Reserve.clicked.connect(lambda: self.Reserve_Clicked())       
+        
+    def GetYear(self):
+        date = self.calendarWidget.selectedDate().toString(QtCore.Qt.ISODate) #date = "YYYY-MM-DD"
+        splitDate = []
+        splitDate = date.split('-') #splitDate = [YYYY,MM,DD]
+        year = splitDate[0]
+        return int(year)
+    
+    def GetMonth(self):
+        date = self.calendarWidget.selectedDate().toString(QtCore.Qt.ISODate)
+        splitDate = []
+        splitDate = date.split('-')
+        month = splitDate[1]
+        return month
+
+    def GetDay(self):
+        date = self.calendarWidget.selectedDate().toString(QtCore.Qt.ISODate)
+        splitDate = []
+        splitDate = date.split('-')
+        day = splitDate[2]
+        return int(day)
+               
     def Reserve_Clicked(self):
         self.newReservation = Reservation()
         self.ReservationInfo()
         
         self.newReservation.KeepReservation()
-        print("CLICKED")
+        self.MessageBox(self, "Schedule Reserved", "Success")
      
     def ReservationInfo(self):
         self.newReservation.SetNatureOfActivity(self.comboBox_NatureOfActivity.currentText())
         self.newReservation.SetOrganization(self.lineEdit_Organization.text())
         self.newReservation.SetRoom(self.comboBox_Room.currentText())
-        #self.newReservation.SetMonth()
-        #self.newReservation.SetDay()
-        #self.newReservation.SetYear()
+        self.newReservation.SetMonth(self.GetMonth())
+        self.newReservation.SetDay(self.GetDay())
+        self.newReservation.SetYear(self.GetYear())
         self.newReservation.SetTimeIn(self.comboBox_timeStart.currentText())
         self.newReservation.SetTimeOut(self.comboBox_timeEnd.currentText())
-"""       
+        
+    #Creates message box    
+    def MessageBox(self, message, windowTitle):
+        msg = QMessageBox()
+        msg.setText(message) #show passed message variable
+        msg.setWindowTitle(windowTitle)
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.exec_()        
+"""      
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
@@ -47,42 +76,4 @@ if __name__ == "__main__":
     widget.show()
     sys.exit(app.exec_())
 """
-=======
-# -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'Login.ui'
-#
-# Created by: PyQt5 UI code generator 5.9.2
-#
-# WARNING! All changes made in this file will be lost! 
-
-from PyQt5.QtWidgets import QApplication, QMessageBox, QMainWindow
-from PyQt5 import QtCore
-from PyQt5.uic import loadUi
-
-class User(QMainWindow):
-    def __init__(self):
-        super(User, self).__init__()
-        loadUi('User.ui',self)
-        self.loginMsg = QMessageBox()
-        self.setWindowTitle('Room Reservation')
-        
-        #When Date is Selected
-        self.calendarWidget.clicked.connect(self.showDate)
-        
-    def showDate(self):
-        print(self.calendarWidget.selectedDate().toString(QtCore.Qt.ISODate))
-        print(self.calendarWidget.lon
-    def reserve_Clicked(self):
-        print("value")
-        
-   
-    
-if __name__ == "__main__":
-    import sys
-    app = QApplication(sys.argv)
-    widget = User()
-    widget.show()
-    sys.exit(app.exec_())
-
->>>>>>> FRADEJAS
