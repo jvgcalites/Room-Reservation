f0a9145dd0473436d7dd175d5a0a56e43f274e84
jvgcalites <jvgcalites2018@gmail.com>
2018-10-02 22:31:19 +0800

    edited User.py
    can now store to database upon clicking reserve button

--- User.py
+++ User.py
@@ -3,6 +3,7 @@
 from Reservation_Filehandling import Reservation_fileHandling
 from PyQt5.QtWidgets import QApplication, QMessageBox, QMainWindow
 from PyQt5.uic import loadUi
+from Reservation import Reservation
 
 class User(QMainWindow):
     def __init__(self, email):
@@ -22,12 +23,26 @@ class User(QMainWindow):
         self.pushButton_Reserve.clicked.connect(lambda: self.Reserve_Clicked())
             
     def Reserve_Clicked(self):
-        print("DAYNE")
+        self.newReservation = Reservation()
+        self.ReservationInfo()
         
+        self.newReservation.KeepReservation()
+        print("CLICKED")
+     
+    def ReservationInfo(self):
+        self.newReservation.SetNatureOfActivity(self.comboBox_NatureOfActivity.currentText())
+        self.newReservation.SetOrganization(self.lineEdit_Organization.text())
+        self.newReservation.SetRoom(self.comboBox_Room.currentText())
+        #self.newReservation.SetMonth()
+        #self.newReservation.SetDay()
+        #self.newReservation.SetYear()
+        self.newReservation.SetTimeIn(self.comboBox_timeStart.currentText())
+        self.newReservation.SetTimeOut(self.comboBox_timeEnd.currentText())
+"""       
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
     widget = User("acsabesamis@mymail.mapua.edu.ph") #Still Testing
     widget.show()
     sys.exit(app.exec_())
-
+"""
