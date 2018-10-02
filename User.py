fa9ebcb39b093ffb475c64dc2d4e05888da8dbbf
Dayne Fradejas <42403284+dnfradejas@users.noreply.github.com>
2018-10-02 22:25:45 +0800

    Add files via upload

--- User.py
+++ User.py
@@ -7,6 +7,7 @@
 # WARNING! All changes made in this file will be lost! 
 
 from PyQt5.QtWidgets import QApplication, QMessageBox, QMainWindow
+from PyQt5 import QtCore
 from PyQt5.uic import loadUi
 
 class User(QMainWindow):
@@ -15,12 +16,17 @@ class User(QMainWindow):
         loadUi('User.ui',self)
         self.loginMsg = QMessageBox()
         self.setWindowTitle('Room Reservation')
-     
-        #Button Events
-        self.pushButton_Reserve.clicked.connect(self.Reserve_Clicked)
-
-    def Reserve_Clicked(self):
-        print("DAYNE")
+        
+        #When Date is Selected
+        self.calendarWidget.clicked.connect(self.showDate)
+        
+    def showDate(self):
+        print(self.calendarWidget.selectedDate().toString(QtCore.Qt.ISODate))
+        print(self.calendarWidget.lon
+    def reserve_Clicked(self):
+        print("value")
+        
+   
     
 if __name__ == "__main__":
     import sys
